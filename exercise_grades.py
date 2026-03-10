e74f792c896d6bbf801bab5619f7bdd5c2ea5159
santinopesce00 <spesce@mail.austral.edu.ar>
2026-03-10 18:15:44 -0300

    Update exercise_grades.py

--- exercise_grades.py
+++ exercise_grades.py
@@ -11,3 +11,11 @@ def grades():
     nota1 = 8
     nota2 = 7
     nota3 = 9
+    promedio = (nota1 + nota2 + nota3) / 3
+    nota_max = max(nota1, nota2, nota3)
+    nota_min = min(nota1, nota2, nota3)
+    promedio_10= 10 - promedio
+    print(promedio)
+    print(nota_max)
+    print(nota_min)
+    print(promedio_10)
